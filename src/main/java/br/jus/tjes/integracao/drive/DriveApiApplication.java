636b6130b1e7fe60dee41f669784b102d6551243
Orlando Resende <orlando.resende@tjdft.jus.br>
2023-07-27 11:10:00 -0300

    Commit de desenvolvimento da solução.

--- src/main/java/br/jus/tjes/integracao/drive/DriveApiApplication.java
+++ src/main/java/br/jus/tjes/integracao/drive/DriveApiApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiGoogleDriveApplication {
+public class DriveApiApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGoogleDriveApplication.class, args);
+		SpringApplication.run(DriveApiApplication.class, args);
 	}
 
 }
