88e6407c63b04d1b8a58b5a5be8a43705a2452c7
Orlando Resende <orlando.resende@tjdft.jus.br>
2023-09-01 11:56:16 -0300

    [LOCAL] Criar projeto básico de springboot.

--- src/main/java/br/jus/tjes/integracao/drive/security/StorageSecurityConfig.java
+++ src/main/java/br/jus/tjes/integracao/drive/security/StorageSecurityConfig.java
@@ -5,11 +5,5 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class StorageSecurityConfig {
-	/*
-	 * @Bean public SecurityFilterChain filterChain(HttpSecurity http) throws
-	 * Exception { http.csrf() .disable() .authorizeRequests()
-	 * .requestMatchers("/**").permitAll();
-	 * 
-	 * return http.build(); }
-	 */
+
 }
